e1e421bca9e5297eadeea6d51069695358867b65
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-06-21 17:27:45 +0100

    basic project initiated and default boilerplates removed

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+	title: "NickOD.xyz | Full-Stack Web Developer",
+	description: "My personal portfolio website",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={poppins.className}>{children}</body>
+		</html>
+	);
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className=" container mx-auto px-1">
+			<h1>Hello from Main 🚀</h1>
+		</main>
+	);
+}
