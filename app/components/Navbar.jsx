8bdda5a19ee199ca61ea3da4af1058f9739f1b00
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-02 10:40:06 +0100

    updated allofyou image, cv & siteUrl link

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import { NavbarLinks, siteUrl } from "./NavbarLinks";
+import { NavbarLinks } from "./NavbarLinks";
 import { title } from "../data/siteTitle";
+import { siteUrl } from "../data/navLinks";
 
 export default function Navbar() {
   return (
     <>
       <header className=" py-6 px-3 fixed z-20 bg-white/95 flex items-center justify-between container">
-        <Link href={{ siteUrl }} className=" text-4xl font-semibold">
+        <Link href={siteUrl} className=" text-4xl font-semibold">
           {title}
         </Link>
         <Link
